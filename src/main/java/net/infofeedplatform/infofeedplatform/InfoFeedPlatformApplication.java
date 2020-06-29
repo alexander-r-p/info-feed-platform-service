c0fbef121757d05b3168ac35ff822f5d2ac3dbce
Oleksandr Romaniuk <>
2020-06-29 12:05:23 +0300

    initial commit

---
+++ src/main/java/net/infofeedplatform/infofeedplatform/InfoFeedPlatformApplication.java
@@ -0,0 +1,13 @@
+package net.infofeedplatform.infofeedplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoFeedPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoFeedPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/infofeedplatform/infofeedplatform/InfoFeedPlatformApplicationTests.java
@@ -0,0 +1,14 @@
+package net.infofeedplatform.infofeedplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//@SpringBootTest
+class InfoFeedPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
